066a9eed7a6d9aac999b6470b5135f408cd9a808
Alex <alex@bradaric.com>
2025-04-05 18:51:03 +0200

    Add unit test for registration form display
    Creates a new test class RegistrationControllerTest that verifies:
    - The registration endpoint responds successfully
    - The registration form is properly rendered with all required fields
    - Input elements for first name, last name, email, and password fields exist

---
+++ tests/Controller/RegistrationControllerTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class RegistrationControllerTest extends WebTestCase
+{
+    public function testRegistrationFormIsDisplayed(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/registracija');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorExists('form[name="registration"]');
+        $this->assertSelectorExists('input[name="registration[firstName]"]');
+        $this->assertSelectorExists('input[name="registration[lastName]"]');
+        $this->assertSelectorExists('input[name="registration[email]"]');
+        $this->assertSelectorExists('input[name="registration[rawPassword][first]"]');
+        $this->assertSelectorExists('input[name="registration[rawPassword][second]"]');
+    }
+}
