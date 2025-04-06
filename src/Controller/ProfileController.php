e9db22a8f5d5127c885613a4ae6cc77c2d4e4507
Darko Todoric <darko.todoric013@gmail.com>
2025-04-06 13:23:34 +0200

    add IsGranted

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -10,8 +10,10 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/profil', name: 'profile_')]
+#[IsGranted('ROLE_USER')]
 class ProfileController extends AbstractController
 {
     public function __construct(private EntityManagerInterface $entityManager)
