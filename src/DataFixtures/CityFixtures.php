698608116cab1bc4919a7cb4013a3dcfa9c4aa66
Darko Todoric <darko.todoric013@gmail.com>
2025-04-06 12:07:29 +0200

    Doctrine Fixtures Bundle

---
+++ src/DataFixtures/CityFixtures.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\City;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
+use Doctrine\Persistence\ObjectManager;
+
+class CityFixtures extends Fixture implements FixtureGroupInterface
+{
+    public function load(ObjectManager $manager): void
+    {
+        $names = ['Beograd', 'Novi Sad', 'Subotica', 'Niš'];
+        foreach ($names as $name) {
+            $city = new City();
+            $city->setName($name);
+            $manager->persist($city);
+        }
+
+        $manager->flush();
+    }
+
+    public static function getGroups(): array
+    {
+        return [1];
+    }
+}
---
+++ src/DataFixtures/SchoolFixtures.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\City;
+use App\Entity\School;
+use App\Entity\SchoolType;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ObjectManager;
+
+class SchoolFixtures extends Fixture implements FixtureGroupInterface
+{
+    public function __construct(private EntityManagerInterface $entityManager)
+    {
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $schools = [[
+            'name' => 'Medicinska škola Beograd',
+            'city' => 'Beograd',
+            'type' => 'Srednja škola'
+        ], [
+            'name' => 'Osnovna škola Oslobodioci Beograda',
+            'city' => 'Beograd',
+            'type' => 'Osnovna škola'
+        ]];
+
+        foreach ($schools as $schoolData) {
+            $school = new School();
+            $school->setName($schoolData['name']);
+
+            $city = $this->entityManager->getRepository(City::class)->findOneBy(['name' => $schoolData['city']]);
+            $school->setCity($city);
+
+            $type = $this->entityManager->getRepository(SchoolType::class)->findOneBy(['name' => $schoolData['type']]);
+            $school->setType($type);
+
+            $manager->persist($school);
+        }
+
+        $manager->flush();
+    }
+
+    public static function getGroups(): array
+    {
+        return [2];
+    }
+}
