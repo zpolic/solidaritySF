9087859b63270a36f36bf8835117bc82209eb6c2
Darko Todoric <darko.todoric013@gmail.com>
2025-04-07 16:33:51 +0200

    admin stats

--- src/Controller/Admin/HomeController.php
+++ src/Controller/Admin/HomeController.php
@@ -2,6 +2,12 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\City;
+use App\Entity\School;
+use App\Entity\SchoolType;
+use App\Entity\User;
+use App\Entity\UserDonor;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,8 +16,29 @@ use Symfony\Component\Routing\Attribute\Route;
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
-        return $this->render('admin/home/index.html.twig');
+        $qb = $entityManager->createQueryBuilder();
+        $totalDelegates = $qb->select('COUNT(u.id)')
+            ->from(User::class, 'u')
+            ->andWhere('u.roles LIKE :role')
+            ->setParameter('role', '%ROLE_DELEGATE%')
+            ->getQuery()
+            ->getSingleScalarResult();;
+
+        $qb = $entityManager->createQueryBuilder();
+        $totalDelegatesSum = $qb->select('SUM(ud.amount)')
+            ->from(UserDonor::class, 'ud')
+            ->getQuery()
+            ->getSingleScalarResult();;
+
+        return $this->render('admin/home/index.html.twig', [
+            'totalDonors' => $entityManager->getRepository(UserDonor::class)->count(),
+            'totalDonorsSum' => $totalDelegatesSum,
+            'totalDelegate' => $totalDelegates,
+            'totalSchool' => $entityManager->getRepository(School::class)->count(),
+            'totalCities' => $entityManager->getRepository(City::class)->count(),
+            'totalUsers' => $entityManager->getRepository(User::class)->count(),
+        ]);
     }
 }
