784fafae4f20b477d8f66d35130ad42029681c8f
Vladimir Lazić <vlazic@gmail.com>
2025-04-08 12:54:52 +0200

    Adds custom 404/500 pages #14 (#16)
    * Adds custom 404/500 pages #14
    
    * Button swapped with daisyui, optimized png images, fixed formatting and added .editorconfig
    
    * Add .editorconfig to .gitignore
    
    * Remove .editorconfig from .gitignore
    
    * Replace SVG icons with Tabler Icons in error templates for consistency
    
    * replace Font Awesome with Tabler Icons

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,5 @@ return [
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Liip\TestFixturesBundle\LiipTestFixturesBundle::class => ['dev' => true, 'test' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
