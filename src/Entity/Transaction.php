1b17f1ac40fbaffff36c8922f441b7b27ab9bd2e
Darko Todorić <7748474+darkotodoric@users.noreply.github.com>
2025-04-16 18:49:12 +0200

    Profile transactions tests (#145)
    * Unit tests for user profile transactions page
    
    * Unit tests for user profile transactions page

--- src/Entity/Transaction.php
+++ src/Entity/Transaction.php
@@ -7,6 +7,7 @@ use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: TransactionRepository::class)]
+#[ORM\Table(name: '`transaction`')]
 #[ORM\HasLifecycleCallbacks]
 class Transaction
 {
