89f8afaec8631bea6b95ea4640c2c11063d7e921
Darko Todorić <7748474+darkotodoric@users.noreply.github.com>
2025-04-21 21:13:14 +0200

    First "dump" the tables (#196)

--- configureProject.sh
+++ configureProject.sh
@@ -25,26 +25,15 @@ docker exec solidarity-php-container php composer.phar install;
 docker exec solidarity-php-container php bin/console doctrine:database:create --if-not-exists;
 
 # Create tables
+docker exec solidarity-php-container php bin/console doctrine:schema:drop --force;
 docker exec solidarity-php-container php bin/console doctrine:schema:update --force;
 
 # Load fixtures in order:
-echo "Loading data fixtures..."
-echo "1. Base data (cities, schools, users)"
 docker exec solidarity-php-container php bin/console doctrine:fixtures:load --group=1 --no-interaction;
-
-echo "2. Delegate requests"
 docker exec solidarity-php-container php bin/console doctrine:fixtures:load --group=2 --append --no-interaction;
-
-echo "3. School assignments"
 docker exec solidarity-php-container php bin/console doctrine:fixtures:load --group=3 --append --no-interaction;
-
-echo "4. Donors"
 docker exec solidarity-php-container php bin/console doctrine:fixtures:load --group=4 --append --no-interaction;
-
-echo "5. Educators"
 docker exec solidarity-php-container php bin/console doctrine:fixtures:load --group=5 --append --no-interaction;
-
-echo "6. Transactions"
 docker exec solidarity-php-container php bin/console doctrine:fixtures:load --group=6 --append --no-interaction;
 
-echo "✓ Done!"
+echo "Done!"
