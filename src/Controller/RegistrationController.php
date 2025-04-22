d158a5b3933914cac605ce51bc84c4d41d7ac76f
Darko Todorić <7748474+darkotodoric@users.noreply.github.com>
2025-04-22 20:46:39 +0200

    Remove registration page (#207)
    * Remove registration page
    
    * cs fixer

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -2,12 +2,9 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
-use App\Form\RegistrationType;
 use App\Repository\UserDonorRepository;
 use App\Repository\UserRepository;
 use App\Security\EmailVerifier;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Request;
@@ -18,10 +15,8 @@ use SymfonyCasts\Bundle\VerifyEmail\Exception\VerifyEmailExceptionInterface;
 
 class RegistrationController extends AbstractController
 {
-    public function __construct(
-        private EmailVerifier $emailVerifier,
-        private EntityManagerInterface $entityManager,
-    ) {
+    public function __construct(private EmailVerifier $emailVerifier)
+    {
     }
 
     #[Route('/ponovna-verifikacija-email', name: 'resend_verification')]
@@ -73,33 +68,6 @@ class RegistrationController extends AbstractController
         $this->addFlash('success', 'Verifikacioni email je ponovo poslat na vašu adresu. Molimo proverite vaš inbox.');
 
         return $this->redirectToRoute('login');
-    }
-
-    #[Route('/registracija', name: 'register')]
-    public function register(Request $request, UserRepository $userRepository): Response
-    {
-        $user = new User();
-        $form = $this->createForm(RegistrationType::class, $user);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->entityManager->persist($user);
-            $this->entityManager->flush();
-
-            $userRepository->sendVerificationLink($user, null);
-
-            return $this->redirectToRoute('verify_email_send');
-        }
-
-        return $this->render('registration/register.html.twig', [
-            'form' => $form,
-        ]);
-    }
-
-    #[Route('/verifikacija-email', name: 'verify_email_send')]
-    public function verifyEmailSend(): Response
-    {
-        return $this->render('registration/verify_email_send.html.twig');
     }
 
     #[Route('/uspesna-verifikacija-emaila', name: 'verify_email_success')]
--- tests/Controller/RegistrationControllerTest.php
+++ tests/Controller/RegistrationControllerTest.php
@@ -2,11 +2,11 @@
 
 namespace App\Tests\Controller;
 
+use App\Entity\User;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-use Symfony\Component\DomCrawler\Crawler;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 class RegistrationControllerTest extends WebTestCase
@@ -65,7 +65,7 @@ class RegistrationControllerTest extends WebTestCase
         $email = 'verified@example.com';
         $this->removeUser($email);
 
-        $userClass = $this->entityManager->getClassMetadata(\App\Entity\User::class)->getName();
+        $userClass = $this->entityManager->getClassMetadata(User::class)->getName();
         $user = new $userClass();
         $user->setFirstName('Verified');
         $user->setLastName('User');
@@ -87,7 +87,7 @@ class RegistrationControllerTest extends WebTestCase
         $email = 'unverified@example.com';
         $this->removeUser($email);
 
-        $userClass = $this->entityManager->getClassMetadata(\App\Entity\User::class)->getName();
+        $userClass = $this->entityManager->getClassMetadata(User::class)->getName();
         $user = new $userClass();
         $user->setFirstName('Unverified');
         $user->setLastName('User');
@@ -107,56 +107,5 @@ class RegistrationControllerTest extends WebTestCase
 
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('.alert.alert-error', 'Molimo sačekajte još');
-    }
-
-    public function testRegistrationAndEmailSendAndVerification(): void
-    {
-        $email = 'korisnik@gmail.com';
-        $this->removeUser($email);
-
-        $crawler = $this->client->request('GET', '/registracija');
-
-        $this->assertResponseIsSuccessful();
-        $this->assertSelectorExists('form[name="registration"]');
-        $this->assertSelectorExists('input[name="registration[firstName]"]');
-        $this->assertSelectorExists('input[name="registration[lastName]"]');
-        $this->assertSelectorExists('input[name="registration[email]"]');
-
-        // Registration
-        $form = $crawler->filter('form[name="registration"]')->form([
-            'registration[firstName]' => 'Dragan',
-            'registration[lastName]' => 'Jovanovic',
-            'registration[email]' => $email,
-        ]);
-
-        $this->client->submit($form);
-
-        // Check email
-        $this->assertEmailCount(1);
-        $mailerMessage = $this->getMailerMessage();
-        $this->assertEmailSubjectContains($mailerMessage, 'Link za verifikaciju email adrese');
-        $this->assertEmailTextBodyContains($mailerMessage, 'Kako bismo potvrdili da je ova email adresa ispravna i da pripada Vama, molimo Vas da kliknete na link ispod');
-
-        // Check are user registered
-        $user = $this->userRepository->findOneBy(['email' => $email]);
-        $this->assertEquals('Dragan', $user->getFirstName());
-        $this->assertEquals('Jovanovic', $user->getLastName());
-        $this->assertTrue($user->isActive());
-        $this->assertFalse($user->isEmailVerified());
-
-        // Extract verified link
-        $crawler = new Crawler($mailerMessage->getHtmlBody());
-        $verifiedLink = $crawler->filter('#link')->attr('href');
-
-        // Click on verified link from email
-        $this->client->request('GET', $verifiedLink);
-        $this->client->followRedirect();
-        $this->assertResponseIsSuccessful();
-
-        // Check if user is logged and verified
-        $user = $this->getLoginUser();
-        $this->assertNotNull($user);
-        $this->assertEquals('korisnik@gmail.com', $user->getEmail());
-        $this->assertTrue($user->isEmailVerified());
     }
 }
