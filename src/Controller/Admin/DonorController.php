090fd9382208247f4cc495f633212ee8bc115e07
Darko Todorić <7748474+darkotodoric@users.noreply.github.com>
2025-04-27 20:06:29 +0200

    Admin edit donors (#284)
    * educator city admin
    
    * admin donor edit/delete
    
    * admin donor edit/delete
    
    * cs fixer

--- src/Controller/Admin/DonorController.php
+++ src/Controller/Admin/DonorController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\UserDonor;
+use App\Form\Admin\DonorEditType;
 use App\Form\Admin\DonorSearchType;
+use App\Form\ConfirmType;
 use App\Repository\UserDonorRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -27,6 +31,54 @@ final class DonorController extends AbstractController
 
         return $this->render('admin/donor/list.html.twig', [
             'donors' => $userDonorRepository->search($criteria, $page),
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'edit')]
+    public function edit(Request $request, EntityManagerInterface $entityManager, UserDonor $userDonor): Response
+    {
+        $form = $this->createForm(DonorEditType::class, $userDonor, [
+            'user' => $userDonor->getUser(),
+        ]);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($userDonor);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Uspešno ste izmenili podatke');
+
+            return $this->redirectToRoute('admin_donor_list');
+        }
+
+        return $this->render('admin/donor/edit.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/{id}/delete', name: 'delete')]
+    public function delete(Request $request, EntityManagerInterface $entityManager, UserDonor $userDonor): Response
+    {
+        $form = $this->createForm(ConfirmType::class, null, [
+            'message' => 'Potvrđujem da želim da obrišem donatora',
+            'submit_message' => 'Potvrdi',
+            'submit_class' => 'btn btn-error',
+        ]);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->remove($userDonor);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Uspešno ste obrišali donatora');
+
+            return $this->redirectToRoute('admin_donor_list');
+        }
+
+        return $this->render('admin/confirm_message.html.twig', [
+            'iconClass' => 'trash',
+            'title' => 'Brisanje donatora #'.$userDonor->getId(),
             'form' => $form->createView(),
         ]);
     }
---
+++ src/Form/Admin/DonorEditType.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Form\Admin;
+
+use App\Entity\UserDonor;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class DonorEditType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'disabled' => true,
+                'mapped' => false,
+                'label' => 'Ime',
+                'data' => $options['user'] ? $options['user']->getFullName() : null,
+            ])
+            ->add('email', TextType::class, [
+                'disabled' => true,
+                'mapped' => false,
+                'label' => 'Email',
+                'data' => $options['user'] ? $options['user']->getEmail() : null,
+            ])
+            ->add('isMonthly', CheckboxType::class, [
+                'required' => false,
+                'label' => 'Mesečna podrška',
+            ])
+            ->add('amount', TextType::class, [
+                'label' => 'Iznos',
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Sačuvaj',
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => UserDonor::class,
+            'user' => null,
+        ]);
+    }
+}
