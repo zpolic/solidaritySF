faaffa433cc9716c9202b823b28f57eae43e884d
Darko Todorić <7748474+darkotodoric@users.noreply.github.com>
2025-04-28 19:28:31 +0200

    test email (#295)

--- src/Command/SendEmailToDonorsCommand.php
+++ src/Command/SendEmailToDonorsCommand.php
@@ -2,11 +2,13 @@
 
 namespace App\Command;
 
+use App\Entity\User;
 use App\Entity\UserDonor;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
@@ -25,10 +27,39 @@ class SendEmailToDonorsCommand extends Command
         parent::__construct();
     }
 
+    protected function configure(): void
+    {
+        $this
+            ->addArgument('test-email', InputArgument::OPTIONAL, 'Email address to send test email instead of real donors');
+    }
+
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
         $io->section('Command started at '.date('Y-m-d H:i:s'));
+
+        $testEmail = $input->getArgument('test-email');
+
+        if ($testEmail) {
+            $output->writeln('Sending TEST email to: '.$testEmail.' at '.date('Y-m-d H:i:s'));
+
+            $user = $this->entityManager->getRepository(User::class)->findOneBy(['email' => $testEmail]);
+            if (empty($user)) {
+                $output->writeln('User not found');
+
+                return Command::FAILURE;
+            }
+
+            $message = (new TemplatedEmail())
+                ->to($testEmail)
+                ->subject('Imamo važne vesti')
+                ->htmlTemplate('email/donor_new_website.html.twig')
+                ->context(['user' => $user]);
+
+            $this->mailer->send($message);
+
+            return Command::SUCCESS;
+        }
 
         while (true) {
             $items = $this->getItems();
