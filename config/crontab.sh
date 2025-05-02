3c1883740934907c83c49bfd6f9c8c7363d0e08a
Darko Todorić <7748474+darkotodoric@users.noreply.github.com>
2025-05-02 18:22:40 +0200

    Notify donors 1 day before transaction expiration (#346)
    * Notify donors 1 day before transaction expiration
    
    * Notify donors 1 day before transaction expiration

--- config/crontab.sh
+++ config/crontab.sh
@@ -3,6 +3,7 @@
 */10 7-21 * * * php /var/www/solidaritySF/bin/console app:transaction:create 10000 > /var/www/solidaritySF/var/log/crontab-transaction-create-`date +\%d-\%m-\%Y`.txt
 0 * * * * php /var/www/solidaritySF/bin/console app:transaction:expired >> /var/www/solidaritySF/var/log/crontab-transaction-expired-`date +\%d-\%m-\%Y`.txt
 0 7 * * * php /var/www/solidaritySF/bin/console app:transaction:notify-delegates >> /var/www/solidaritySF/var/log/crontab-transaction-notify-delegates-`date +\%d-\%m-\%Y`.txt
+0 7 * * * php /var/www/solidaritySF/bin/console app:transaction:notify-donors >> /var/www/solidaritySF/var/log/crontab-transaction-notify-donors-`date +\%d-\%m-\%Y`.txt
 
 # Create damaged educator period
 0 1 23 * * php /var/www/solidaritySF/bin/console app:create-damaged-educator-period `date -d' -1 month' +\%n` `date -d' -1 month' +\%Y` full >> /var/www/solidaritySF/var/log/crontab-cancelled-transaction-`date +\%d-\%m-\%Y`.txt
---
+++ src/Command/Transaction/NotifyDonorsCommand.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Command\Transaction;
+
+use App\Entity\Transaction;
+use App\Entity\User;
+use App\Entity\UserDonor;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Lock\LockFactory;
+use Symfony\Component\Lock\Store\FlockStore;
+use Symfony\Component\Mailer\MailerInterface;
+
+#[AsCommand(
+    name: 'app:transaction:notify-donors',
+    description: 'Notify donors 1 day before transaction expiration',
+)]
+class NotifyDonorsCommand extends Command
+{
+    private int $lastId = 0;
+
+    public function __construct(private EntityManagerInterface $entityManager, private MailerInterface $mailer)
+    {
+        parent::__construct();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $store = new FlockStore();
+        $factory = new LockFactory($store);
+        $lock = $factory->createLock($this->getName(), 0);
+        if (!$lock->acquire()) {
+            return Command::FAILURE;
+        }
+
+        $io = new SymfonyStyle($input, $output);
+        $io->section('Command started at '.date('Y-m-d H:i:s'));
+
+        $donors = $this->getDonors();
+        foreach ($donors as $donor) {
+            if (!$this->hasTransactionsFromYesterday($donor)) {
+                continue;
+            }
+
+            $output->writeln('Send email to '.$donor->getUser()->getEmail());
+            $this->sendEmail($donor->getUser());
+        }
+
+        $io->success('Command finished at '.date('Y-m-d H:i:s'));
+
+        return Command::SUCCESS;
+    }
+
+    public function hasTransactionsFromYesterday(UserDonor $userDonor): bool
+    {
+        $stmt = $this->entityManager->getConnection()->executeQuery('
+            SELECT COUNT(*)
+            FROM transaction AS t
+            WHERE t.user_id = :userId
+             AND t.status = :status
+             AND DATE(t.created_at) = DATE(NOW() - INTERVAL 1 DAY)
+            ', [
+            'userId' => $userDonor->getUser()->getId(),
+            'status' => Transaction::STATUS_NEW,
+        ]);
+
+        return (bool) $stmt->fetchOne();
+    }
+
+    public function sendEmail(User $user): void
+    {
+        $message = (new TemplatedEmail())
+            ->to($user->getEmail())
+            ->subject('Podsetnik: Instrukcije za uplatu donacije uskoro ističu')
+            ->htmlTemplate('email/transaction-notify-donor.html.twig')
+            ->context([
+                'user' => $user,
+            ]);
+
+        try {
+            $this->mailer->send($message);
+        } catch (\Exception $exception) {
+        }
+    }
+
+    public function getDonors(): array
+    {
+        $qb = $this->entityManager->createQueryBuilder();
+
+        $qb->select('ud')
+            ->from(UserDonor::class, 'ud')
+            ->innerJoin('ud.user', 'u')
+            ->andWhere('u.isActive = 1')
+            ->andWhere('u.isEmailVerified = 1')
+            ->andWhere('u.id > :lastId')
+            ->setParameter('lastId', $this->lastId)
+            ->orderBy('u.id', 'ASC')
+            ->setMaxResults(100);
+
+        $results = $qb->getQuery()->getResult();
+        if (!empty($results)) {
+            $last = end($results);
+            $this->lastId = $last->getId();
+        }
+
+        return $results;
+    }
+}
