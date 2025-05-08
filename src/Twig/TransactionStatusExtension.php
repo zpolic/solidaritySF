2adaa26677a2345230f01191f74c432dc4e11cb2
SelenaHocevar <57287910+SelenaHocevar@users.noreply.github.com>
2025-05-08 13:26:57 +0200

    374 new icons (#377)
    * [#374]feat: Update icons and text on the status codes page
    
    * [#374] fix: php styling issue
    
    * fix: ru php-cs-fixer
    
    * [#374] fix: remove bold styling from status labels

--- src/Twig/TransactionStatusExtension.php
+++ src/Twig/TransactionStatusExtension.php
@@ -36,6 +36,6 @@ class TransactionStatusExtension extends AbstractExtension
 
         $icon = ' <img src="'.$iconPath.'" alt="'.$statusName.'" class="w-5 h-5 inline-block mr-1.5 relative -translate-y-0.5" />';
 
-        return $icon.' <span class="font-medium">'.$statusName.'</span>';
+        return $icon.' <span>'.$statusName.'</span>';
     }
 }
