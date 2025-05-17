6c7fc885be32e53cc7a242a6f1debf64beca5387
Zoran Polić <zoran.polic@omnilogika.com>
2025-05-17 18:08:38 +0200

    resend email as hidden param in post, controller update, flashes update (#370)

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -20,37 +20,87 @@ class SecurityController extends AbstractController
         }
 
         if ($request->isMethod('POST')) {
+            
             $captchaToken = $request->getPayload()->get('cf-turnstile-response');
-            if (!$cloudFlareTurnstileService->isValid($captchaToken)) {
-                $this->addFlash('error', 'Captcha nije validna.');
-
-                return $this->redirectToRoute('login');
+            $emailresend = $request->getPayload()->get('emailresend');
+            
+            if(!$emailresend){  
+                if (!$cloudFlareTurnstileService->isValid($captchaToken)) {
+                    $this->addFlash('error', 'Captcha nije validna.');
+                    
+                    return $this->redirectToRoute('login');
+                }
+                
+                $email = $request->getPayload()->get('email');
+                $user = $userRepository->findOneBy(['email' => $email]);
+    
+                if ($user && $user->isActive() && $user->isEmailVerified()) {
+                    $userRepository->sendLoginLink($user);
+    
+                    $this->addFlash('success', 'Link za prijavu je poslat na vašu email adresu.');
+    
+                    return $this->redirectToRoute('login');
+                }
+    
+                if ($user && !$user->isActive()) {
+                    $this->addFlash('error', 'Korisnik sa ovom email adresom nije aktivan i ne može da se uloguje.');
+    
+                    return $this->redirectToRoute('login');
+                }
+    
+                if ($user && !$user->isEmailVerified()) {
+                    $this->addFlash('unverified_user', $email);
+    
+                    return $this->redirectToRoute('login');
+                }
+                
+                $this->addFlash('error', 'Korisnik sa ovom email adresom ne postoji. Molimo da se registrujete.');
+            
+            } else {
+                if (!$cloudFlareTurnstileService->isValid($captchaToken)) {
+                    $this->addFlash('error', 'Captcha nije validna.');
+                    $this->addFlash('unverified_user_no_alert', $emailresend);
+                    return $this->redirectToRoute('login');
+                }
+                
+                $user = $userRepository->findOneBy(['email' => $emailresend]);
+                if (!$user) {
+                    $this->addFlash('error', 'Korisnik sa ovom email adresom ne postoji.');
+                    
+                    return $this->redirectToRoute('login');
+                }
+                
+                if ($user->isEmailVerified()) {
+                    $this->addFlash('success', 'Vaš nalog je već potvrđen. Možete se prijaviti.');
+                    
+                    return $this->redirectToRoute('login');
+                }
+                
+                $session = $request->getSession();
+                $lastResendKey = 'last_verification_resend_'.$emailresend;
+                $lastResend = $session->get($lastResendKey);
+                $now = new \DateTime();
+                
+                if ($lastResend) {
+                    $minutesSinceLastResend = ($now->getTimestamp() - $lastResend) / 60;
+                    if ($minutesSinceLastResend < 5) {
+                        $this->addFlash(
+                            'error',
+                            sprintf(
+                                'Molimo sačekajte još %d minuta pre nego što ponovo pošaljete link za potvrdu email adrese.',
+                                ceil(5 - $minutesSinceLastResend)
+                                )
+                            );
+                        $this->addFlash('unverified_user_no_alert', $emailresend);
+                        return $this->redirectToRoute('login');
+                    }
+                }
+                
+                $userRepository->sendVerificationLink($user, null);
+                $session->set($lastResendKey, $now->getTimestamp());
+                
+                $this->addFlash('success', 'Link za potvrdu email je ponovo poslat na vašu adresu. Molimo proverite vaš inbox.');
             }
-
-            $email = $request->getPayload()->get('email');
-            $user = $userRepository->findOneBy(['email' => $email]);
-
-            if ($user && $user->isActive() && $user->isEmailVerified()) {
-                $userRepository->sendLoginLink($user);
-
-                $this->addFlash('success', 'Link za prijavu je poslat na vašu email adresu.');
-
-                return $this->redirectToRoute('login');
-            }
-
-            if ($user && !$user->isActive()) {
-                $this->addFlash('error', 'Korisnik sa ovom email adresom nije aktivan i ne može da se uloguje.');
-
-                return $this->redirectToRoute('login');
-            }
-
-            if ($user && !$user->isEmailVerified()) {
-                $this->addFlash('unverified_user', $email);
-
-                return $this->redirectToRoute('login');
-            }
-
-            $this->addFlash('error', 'Korisnik sa ovom email adresom ne postoji. Molimo da se registrujete.');
 
             return $this->redirectToRoute('login');
         }
