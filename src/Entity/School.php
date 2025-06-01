ccd38b26ae15764561131cff101b6982d30d2c19
Darko Todoric <darko.todoric013@gmail.com>
2025-06-01 13:33:46 +0200

    school processing

--- src/Entity/School.php
+++ src/Entity/School.php
@@ -10,7 +10,7 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: SchoolRepository::class)]
-#[ORM\Index(name: 'idx_processing', columns: ['processing'])]
+#[ORM\Index(name: 'idx_school_processing', columns: ['processing'])]
 #[ORM\HasLifecycleCallbacks]
 class School
 {
